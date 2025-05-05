bd41282950e2fd008038163f0e2cb3d413b8525a
shox404 <thesh8080@gmail.com>
2025-05-05 18:02:52 +0500

    Update

--- client/app/socket.ts
+++ client/app/socket.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 const socket = io(
-  process.env.WEB_TYPE === "production"
+  process.env.NEXT_PUBLIC_WEB_TYPE === "production"
     ? "https://realtime-project-firebase-api.vercel.app"
     : "http://localhost:4000"
 );
