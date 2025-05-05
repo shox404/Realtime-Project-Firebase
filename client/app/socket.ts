f7df9b6f2b5334ccdc27cf4ae93719ad7e3632b2
shox404 <thesh8080@gmail.com>
2025-05-06 01:33:31 +0500

    Update

--- client/app/socket.ts
+++ client/app/socket.ts
@@ -3,11 +3,11 @@ import { io } from "socket.io-client";
 const isBrowser = typeof window !== "undefined";
 const isLocalhost = isBrowser && window.location.hostname === "localhost";
 
-export const SOCKET_URL = isLocalhost
-  ? `http://localhost:4000`
-  : "https://realtime-project-firebase-api.vercel.app";
+// export const SOCKET_URL = isLocalhost
+//   ? `http://localhost:4000`
+//   : "https://realtime-project-firebase-api.vercel.app";
 
-const socket = io(SOCKET_URL, {
+const socket = io("http://localhost:4000", {
   transports: ["websocket"],
   reconnectionAttempts: 5,
   timeout: 10000,
